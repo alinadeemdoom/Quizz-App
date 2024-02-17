e4007aee4cbccb8d8acef2a9a2fe1a665d54a365
Ali Nadeem <ali.nadeem20023@gmail.com>
2024-02-17 17:35:50 +0500

    hello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JoinPageComponent } from './pages/join-page/join-page.component';
+import { InfoPageComponent } from './pages/info-page/info-page.component';
+import { QuizPageComponent } from './pages/quiz-page/quiz-page.component';
+import { ResultPageComponent } from './pages/result-page/result-page.component';
 
-const routes: Routes = [];
+const routes: Routes= [
+  { path: '', component: JoinPageComponent },
+  { path:'quiz-info', component: InfoPageComponent},
+  { path: 'quiz', component: QuizPageComponent},
+  { path: 'quiz-score', component: ResultPageComponent}
+]
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JoinPageComponent } from './pages/join-page/join-page.component';
+import { InfoPageComponent } from './pages/info-page/info-page.component';
+import { QuizPageComponent } from './pages/quiz-page/quiz-page.component';
+import { ResultPageComponent } from './pages/result-page/result-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JoinPageComponent,
+    InfoPageComponent,
+    QuizPageComponent,
+    ResultPageComponent
   ],
   imports: [
     BrowserModule,
